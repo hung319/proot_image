3bd0c18cc9a6804463e65e207d9b6a2a08860d81
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-08-21 08:05:48 +0700

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+ROOTFS_DIR=$(pwd)
+PROOT_VERSION="5.3.0"
+
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH_PD="x86_64"
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH_PD="aarch64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+OS_VERSION="43"
+IMAGE_URL="https://github.com/fedora-cloud/docker-brew-fedora/raw/refs/heads/${OS_VERSION}/${ARCH_PD}/fedora-20250817.tar"
+
+mkdir -p tmp
+if [ -e "$ROOTFS_DIR/.installed" ]; then
+    echo "OS đã được cài rồi, skip bước cài đặt"
+else
+    echo "[*] Đang tải rootfs..."
+    curl -Lo ./tmp/rootfs.tar "$IMAGE_URL"
+
+    mkdir -p "$ROOTFS_DIR"
+    tar -xvf ./tmp/rootfs.tar -C "$ROOTFS_DIR"
+
+    mkdir -p $ROOTFS_DIR/usr/local/bin
+    echo "[*] Đang tải proot..."
+    curl -Lo "$ROOTFS_DIR/usr/local/bin/proot" \
+        "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
+    chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
+
+    echo "[*] Set DNS"
+    echo "nameserver 1.1.1.1" > "$ROOTFS_DIR/etc/resolv.conf"
+    echo "nameserver 1.0.0.1" >> "$ROOTFS_DIR/etc/resolv.conf"
+
+    echo "[*] Cleanup..."
+    rm -f ./tmp/rootfs.tar
+
+    touch "$ROOTFS_DIR/.installed"
+fi
+
+clear && cat << "EOF"
+Welcome to Ubuntu rootfs!
+EOF
+
+"$ROOTFS_DIR/usr/local/bin/proot" \
+    --rootfs="$ROOTFS_DIR" \
+    --link2symlink \
+    --kill-on-exit \
+    --root-id \
+    --cwd=/root \
+    --bind=/proc \
+    --bind=/dev \
+    --bind=/sys \
+    --bind=/tmp \
+    /bin/bash
